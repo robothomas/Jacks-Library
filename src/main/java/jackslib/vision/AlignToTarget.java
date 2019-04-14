cb0aff8c5191fa8d189403ebeb7e2195116d9e58
Dylan <robothomas@users.noreply.github.com>
2019-04-13 20:38:48 -0500

    Add quick vision stub

---
+++ src/main/java/jackslib/vision/AlignToTarget.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package jackslib.vision;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+/**
+ * Add your docs here.
+ */
+public class AlignToTarget {
+
+  private SpeedController leftMaster;
+  private SpeedController rightMaster;
+
+  public void setOutput() {}
+
+  public void adjustToPosition() {
+
+  }
+
+}
